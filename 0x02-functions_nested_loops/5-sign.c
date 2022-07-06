4715b25bc72eb301d410afa389840c9482314c9e
OsbeeAsamoah <asamoahosborn4@gmail.com>
2022-07-05 18:49:16 -0700

    Task 5 to 7

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,7 @@
 int print_sign(int n)
 {
 int numz;
+
 if (n > 0)
 {
 numz = 1;
@@ -18,12 +19,9 @@ numz = 0;
 _putchar('0');
 }
 else
-{    
+{
 numz = -1;
-        
 _putchar('-');
-    
 }
-    
 return (numz);
 }
